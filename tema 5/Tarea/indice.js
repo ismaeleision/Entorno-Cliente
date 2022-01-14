50165183421312c73438cbd7fa3eacaf57b37aae
ismaeleision <ismaeleision@gmail.com>
2022-01-14 21:46:36 +0100

    14/01/2022

--- tema 5/Tarea/indice.js
+++ tema 5/Tarea/indice.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   //Disparadores del Nombre
   let nombre1 = document
     .getElementById('nom')
-    .addEventListener('blur', mayusculas);
+    .addEventListener('blur', mayusculasNombre);
 
   let nombre2 = document
     .getElementById('nom')
@@ -11,7 +11,7 @@ window.addEventListener('load', function () {
   //Disparadores del Apellido
   let apellido1 = document
     .getElementById('apellidos')
-    .addEventListener('blur', mayusculas);
+    .addEventListener('blur', mayusculasApellidos);
 
   let apellido2 = document
     .getElementById('apellidos')
@@ -36,17 +36,69 @@ window.addEventListener('load', function () {
     .addEventListener('blur', validarFecha);
 });
 
-function mayusculas() {
-  console.log('zah');
+function mayusculasNombre() {
+  let nombre = document.getElementById('nom').value;
+  nombre = nombre.split('');
+  for (let i = 0; i < nombre.length; i++) {
+    if (i == 0) {
+      nombre[i] = nombre[i].toUpperCase();
+    } else {
+      nombre[i] = nombre[i].toLowerCase();
+    }
+  }
+  nombre = nombre.join('');
+  console.log(nombre);
+  document.getElementById('nom').value = nombre;
+}
+
+function mayusculasApellidos() {
+  let nombre = document.getElementById('apellidos').value;
+  nombre = nombre.split('');
+  for (let i = 0; i < nombre.length; i++) {
+    if (i == 0) {
+      nombre[i] = nombre[i].toUpperCase();
+    } else {
+      nombre[i] = nombre[i].toLowerCase();
+    }
+    nombre = nombre.join('');
+    document.getElementById('apellidos').value = nombre;
+  }
 }
 
 function comprobacionNombre() {
-  console.log('zah');
+  let exp = /(?=.*\d)/;
+  let nombre = document.getElementById('nom').value;
+  if (exp.test(nombre) || nombre.length <= 0) {
+    document.getElementById('nom').style.borderColor = 'red';
+  } else if (nombre.length > 0) {
+    document.getElementById('nom').style.borderColor = 'green';
+  }
 }
 
-function comprobacionApellidos() {}
+function comprobacionApellidos() {
+  let exp = /(?=.*\d)/;
+  let nombre = document.getElementById('apellidos').value;
+  if (exp.test(nombre) || nombre.length <= 0) {
+    document.getElementById('apellidos').style.borderColor = 'red';
+  } else {
+    document.getElementById('apellidos').style.borderColor = 'green';
+  }
+}
 
-function validarContraseña() {}
+function validarContraseña() {
+  let exp = /^(?=.*\d)(?=.*[.;,-])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  let con = document.getElementById('contraseña').value;
+  let x = exp.test(con);
+  if (!x) {
+    // document.getElementById('reContraseña').style.background = 'red';
+    document.getElementById('contraseña').style.borderColor = 'red';
+
+    //Si coinciden se pone el fondo blanco y el borde verde
+  } else {
+    //document.getElementById('reContraseña').style.background = 'white';
+    document.getElementById('contraseña').style.borderColor = 'green';
+  }
+}
 
 function validarReContraseña() {
   //se coge la informacion de cada input
